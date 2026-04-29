dd66d5b84c9a45f13757f149b4b280ec2fc48666
inesuribeb <inesuribeb@gmail.com>
2026-04-30 00:35:12 +0200

    movil prtocolos

--- components/protocolos/hero.php
+++ components/protocolos/hero.php
@@ -81,7 +81,7 @@ $current_url = get_permalink( get_the_ID() );
     <!-- Nav móvil -->
     <div class="pr-hero__movil-top">
         <button class="pr-hero__movil-btn" aria-expanded="false">
-            + Protocolos
+            Protocolos
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
             </svg>
