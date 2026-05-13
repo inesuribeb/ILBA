f445012f85f5a1c7513dc33c5af0fe1bf38036c4
inesuribeb <inesuribeb@gmail.com>
2026-05-13 12:26:32 +0200

    eventos boton consultar disponibiñidad

--- single-eventos.php
+++ single-eventos.php
@@ -4,6 +4,16 @@
  */
 
 get_header();
+
+// Comprobamos si el evento ya pasó
+$fecha_evento = get_field( 'ev_fecha' );
+$es_pasado    = false;
+
+if ( $fecha_evento ) {
+    $ts_evento = DateTime::createFromFormat( 'd/m/Y', $fecha_evento )->getTimestamp();
+    $ts_hoy    = DateTime::createFromFormat( 'd/m/Y', date( 'd/m/Y' ) )->getTimestamp();
+    $es_pasado = $ts_evento < $ts_hoy;
+}
 ?>
 
 <main id="main" class="single-eventos">
@@ -13,7 +23,9 @@ get_header();
     <?php get_template_part( 'components/eventos/intro' ); ?>
     <?php get_template_part( 'components/eventos/seccion2' ); ?>
 
-    <a href="<?php echo esc_url( get_permalink( get_page_by_path( 'contacto' ) ) ); ?>" class="ev-cta-flotante">Consulta disponibilidad</a>
+    <?php if ( ! $es_pasado ) : ?>
+        <a href="<?php echo esc_url( get_permalink( get_page_by_path( 'contacto' ) ) ); ?>" class="ev-cta-flotante">Consulta disponibilidad</a>
+    <?php endif; ?>
 
 </main>
 
