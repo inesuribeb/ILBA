2366fec97879c93d822abc38440789518dd649c9
inesuribeb <inesuribeb@gmail.com>
2026-05-13 13:07:21 +0200

    intenando solucionar estilos globales en legal css

--- page.php
+++ page.php
@@ -5,17 +5,13 @@
 get_header();
 ?>
 
-<main class="page-default">
-    <div class="page-default__container">
-        <?php while ( have_posts() ) : the_post(); ?>
-            <article class="page-default__content">
-                <h1 class="page-default__title"><?php the_title(); ?></h1>
-                <div class="page-default__body">
-                    <?php the_content(); ?>
-                </div>
-            </article>
-        <?php endwhile; ?>
-    </div>
+<main class="legal-page">
+    <?php while ( have_posts() ) : the_post(); ?>
+        <h1 class="legal-page__title"><?php the_title(); ?></h1>
+        <div class="legal-page__body">
+            <?php the_content(); ?>
+        </div>
+    <?php endwhile; ?>
 </main>
 
 <?php get_footer(); ?>
